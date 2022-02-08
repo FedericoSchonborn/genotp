0381cbad9f2bf175c8f7bd8dc228eaa28088ed81
Federico Damián Schonborn <fdschonborn@gmail.com>
2022-02-07 21:17:55 -0300

    Print usage if no secret was given
    Signed-off-by: Federico Damián Schonborn <fdschonborn@gmail.com>

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/pquerna/otp/totp"
@@ -10,12 +11,17 @@ import (
 
 func main() {
 	if err := run(os.Args); err != nil {
-		fmt.Fprintf(os.Stderr, "gotp: %v", err)
+		fmt.Fprintf(os.Stderr, "gotp: %v\n", err)
 		os.Exit(1)
 	}
 }
 
 func run(args []string) error {
+	if len(args) < 2 {
+		fmt.Fprintf(os.Stderr, "usage: %s <SECRET>\n", filepath.Base(args[0]))
+		os.Exit(2)
+	}
+
 	code, err := totp.GenerateCode(args[1], time.Now())
 	if err != nil {
 		return err
